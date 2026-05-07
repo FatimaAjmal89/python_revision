361466210985540f076b259e2f738f1a92c865cd
FatimaAjmal89 <fatimaajmal899@gmail.com>
2026-05-07 23:31:16 +0500

    functions practice

--- functions.py
+++ functions.py
@@ -5,3 +5,28 @@ def box(a,b,c):
     return avg
 
 box(1,2,4)
+
+def box(a,b,c=9):
+    sum = a + b + c
+    avg = sum/3
+    return avg
+
+print(box(1,2))
+
+
+def factoiral (n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factoiral(n - 1)
+
+
+print(factoiral(6))
+
+
+#lambda functions
+sum  = lambda a,b : a+b
+print(sum(2,4))
+
+avg  = lambda a,b : (a+b)/2
+print(avg(2,4))
