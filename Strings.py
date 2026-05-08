27614c1b274a6757123aee9ce078d6e2d9bd4740
FatimaAjmal89 <fatimaajmal899@gmail.com>
2026-05-08 14:16:06 +0500

    string formating

--- Strings.py
+++ Strings.py
@@ -6,3 +6,16 @@ print(word1[2])
 word2 = " she happy"
 print(word2[5:10])
 
+#formating for dynamic string
+#normal formating
+h=90
+f=91
+sum = h+f
+print("the sum of {} and {} is {}".format(h,f,sum))
+
+#index based formating
+print("the sum of {1} and {0} is {2}".format(h,f,sum))
+
+# value based formating
+print(" {1} and {0}".format(2,8))
+
