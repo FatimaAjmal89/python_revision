3b8c33ec24a6c5ddb42186bf4e5ea4933dc4a632
FatimaAjmal89 <fatimaajmal899@gmail.com>
2026-05-10 14:47:08 +0500

    rename

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,23 +1,41 @@
-class Book:
-    review = 0
-    def __init__(self,title,author):
-        self.title = title
-        self.author = author
-        self.review = []
+class Student:
+    def __init__(self,name,roll_no , marks):
+        self.__name = name
+        self.__roll_no = roll_no
+        self.__marks = marks
 
-    def new_review(self,review):
-        self.review.append(review)
 
-    def count_review(self):
+    def get_name(self):
+        return self.__name
 
-        print(f"total reviews {len(self.review) }")
+    def get_rollno(self):
+        return self.__roll_no
 
-    def display(self):
-        for r in self.review:
-            print(f"review : { r}")
+    def get_mark(self):
+        return self.__marks
 
-b1 = Book ("dumbo" ,"huzaifa" )
-b1.new_review("good")
-b1.new_review("gooddddd")
-b1.count_review()
-b1.display()
+    def set_name(self,name):
+        if(name != ""):
+          self.__name = name
+
+    def set_roll_no(self,rollno):
+        if (rollno>=1 and rollno<=100):
+           self.__roll_no = rollno
+        else:
+            print("invalid rollno")
+
+    def set_marks(self,marks):
+        if (marks <=0 ):
+            print("invalid")
+        else:
+           self.__marks=  marks
+    def info(self):
+        print(f"student name = {self.__name} , {self.__marks} , {self.__roll_no}"
+              )
+
+s1 = Student("faa",1010,30)
+s1.set_name("fata")
+s1.info()
+s1.set_marks(70)
+s1.info()
+print(s1.get_mark())
